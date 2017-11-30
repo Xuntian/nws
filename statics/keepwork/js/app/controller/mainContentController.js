9c31cd2c5344e62a65bef0a63ef3c42f032547e4
Xuntian <li.zq@foxmail.com>
2017-11-30 11:18:26 +0800

    add pages

--- statics/keepwork/js/app/controller/mainContentController.js
+++ statics/keepwork/js/app/controller/mainContentController.js
@@ -4,38 +4,7 @@ define([
 ], function (app, htmlContent) {
    
     app.registerController("mainContentController", ['$rootScope','$scope', function ($rootScope, $scope) {
-        $scope.asd = "asd";	
-		$scope.stageView = "manager";	
-		$scope.showManagerView = function(){
-			$scope.stageView = "manager";
-		}
-		$scope.showUserView = function(){
-			$scope.stageView = "user";
-		}
-		$scope.showWebsiteView = function(){
-			$scope.stageView = "website";
-		}
-		$scope.showDomainView = function(){
-			$scope.stageView = "domain";
-		}
-		$scope.showVIPView = function(){
-			$scope.stageView = "VIP";
-		}
-		$scope.showBigFileView = function(){
-			$scope.stageView = "bigfile";
-		}
-		$scope.showSensitiveWordsView = function(){
-			$scope.stageView = "sensitivewords";
-		}
-		$scope.showOauthView = function(){
-			$scope.stageView = "oauth";
-		}
-		$scope.showTemplateView = function(){
-			$scope.stageView = "template";
-		}
-		$scope.showDemoView = function(){
-			$scope.stageView = "demo";
-		}
+        
     }]);
 
     return htmlContent;
