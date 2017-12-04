af2e282f083dede6401d1154d1175d311df9466d
Xuntian <li.zq@foxmail.com>
2017-12-04 03:47:29 -0800

    modify index.lua

--- model/admin.lua
+++ model/admin.lua
@@ -7,8 +7,8 @@ local admin = nws.inherit(orm)
 
 admin:tablename("admin")                   -- 表名
 --demo:addfield("demo_id", "number", "ID")       -- 主键id
-admin:addfield("username", "string", "用户名")      -- 用户名字段
-admin:addfield("password", "string", "密码")      -- 密码字段
+admin:addfield("username", "string", "username")      -- 用户名字段
+admin:addfield("password", "string", "password")      -- 密码字段
 admin:addfield("email", "string", "email")  
 admin:addfield("cellphone", "string", "cellphone")  
 admin:addfield("privilege", "number", "privilege level")  
